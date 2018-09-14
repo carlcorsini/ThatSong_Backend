c0ad6c663d2cebac1f197cf886610bab4c1c3c1e
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-09-14 13:18:05 -0700

    all routes tested

---
+++ test/response.js
@@ -0,0 +1,15 @@
+class Response {
+  constructor() {}
+
+  status(status) {
+    // console.log('test status 200')
+    return this
+  }
+
+  json() {
+    // console.log('test response .json()')
+    return this
+  }
+}
+
+module.exports = Response
