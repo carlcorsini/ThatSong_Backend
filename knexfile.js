3bd9239d7e1bb55053724714f28fbfd307a0c6cf
Carl Corsini <carl.m.corsini@gmail.com>
2018-09-24 16:06:40 -0700

    added postgres database url

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,8 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: '',
+    connection:
+      'postgres://hnvtctzpywiujn:2937e5a14d4dacda9e53c64eec494bd24f84911f9dd7d605f5461e6258e72029@ec2-54-235-86-226.compute-1.amazonaws.com:5432/dcpds7vg55uoo4',
     migrations: {
       directory: './db/migrations'
     },
